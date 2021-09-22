066d35c21bcdecb808f21052aab4f10e1608e2e1
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-09-22 13:21:01 +0200

    Second task - Gmeplay and randomness

---
+++ Exercise_02-PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float counter = 0f;
+    private float spawnInterval = 1f; 
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && counter >= spawnInterval)
+        {
+            //Podemos generar un nuevo perro
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Debug.LogFormat("Tiempo de espera entre disparos establecido en {0} segundos", spawnInterval);
+            counter = 0;
+        }
+    }
+}
---
+++ Exercise_02-PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+    private float counter = 0f;
+    private float spawnInterval = 2f;
+    private void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= spawnInterval)
+        {
+            //Generamos una nueva bola
+            SpawnRandomBall();
+            Debug.LogFormat("Hemos esperado {0} segundos", spawnInterval);
+            counter = 0;
+            spawnInterval = Random.Range(2, 5);
+        }
+    }
+}
